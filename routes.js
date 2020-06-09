a185b755dc122c82e7d7d3586603ec61f4b01988
vijay0405 <uvijaysimha@gmail.com>
2020-06-09 23:48:33 +0530

    added grant token route

--- routes.js
+++ routes.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const config = require('./config')
+const request = require('request');
+
 
 router.get('/grant/token', (req, res) => {
     console.log('/grant/token');
+    const urlGrant = `https://accounts.zoho.in/oauth/v2/auth` +
+        `?scope=${config.scope}` +
+        `&client_id=${config.clientId}` +
+        `&response_type=code&access_type=offline` +
+        `&redirect_uri=${config.redirectUri}` +
+        `&prompt=consent`;
+
+    res.json({
+        'visit this url': urlGrant,
+        'instructions': 'You will be redirected to the "redirectUri" that you specified during registration of the app.' +
+          'Note down the "code={grant_token}" parameter.' +
+          'This is a short-lived token (valid only for a minute) and will be used to generate the access token and refresh token.'
+      });
 });
 
 router.get('/get/tokens/:grant', (req, res) => {
